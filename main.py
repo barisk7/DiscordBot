145d90909c550d63a9b4758b71eb30abed3239b3
Muhammet Onur Bayraktar <mobyrktr@gmail.com>
2019-09-18 23:12:29 +0300

    bu hata ne

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print('hello world')
-print("gavat batu")
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('NjIzOTYyNDkyMjEyNjA5MDQ0.XYKOhg.MhiMX8ky5BtF4KkTazz-GrjZn9c')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(token)

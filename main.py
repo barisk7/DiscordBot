3ed5b405ca1f9d1eae1ac7f32c93658c8055d6e7
batuhan <batuhanyndny@gmail.com>
2019-09-19 00:08:33 +0300

    fixes

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+
+from discord.voice_client import VoiceClient
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
 
-@client.event
+startup_ext = ['Music']
+
+bot = commands.Bot('.')
+
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    print(
-        f'{client.user} has connected to Discord!'
-        f'{guild.name}(id: {guild.id})'
-        )
+    print('Bot ready')
 
-client.run(TOKEN)
+class main_commands():
+    def __init__(self,bot):
+        self.bot = bot
+
+@bot.command(pass_context=True)
+async def sa(ctx):
+    await bot.say("as")
+
+if __name__ == '__main__':
+    for ext in startup_ext:
+        try:
+            bot.load_extension(ext)
+        except Exception as e:
+            print('failed to load ext')
+
+
+bot.run(TOKEN)
